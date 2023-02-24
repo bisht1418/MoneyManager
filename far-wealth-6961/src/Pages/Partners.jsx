28458b4d41dd3be6c10ff18043a50efc4668ba47
Neeraj <bishtnrj1418@gmail.com>
2023-02-24 22:14:08 +0530

    partner_page

--- far-wealth-6961/src/Pages/Partners.jsx
+++ far-wealth-6961/src/Pages/Partners.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { Text, Box } from "@chakra-ui/react";
+import Multistep from "../Component/Forms";
+import Simple from "../Component/Details";
+
+import { Image, Stack } from "@chakra-ui/react";
 
 const Partners = () => {
-  return <Box>partners</Box>;
+  return (
+    <Box>
+      <Simple />
+      <Stack direction="row">
+        <Image src="../Images/16.png" alt="Dan Abramov" />
+      </Stack>
+
+      <Multistep />
+    </Box>
+  );
 };
 
 export default Partners;
